d861daccdf6ae061a5445846b16991cbee8a2aa1
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2023-07-06 12:04:50 -0300

    added observer

---
+++ observer/ts/observer.ts
@@ -0,0 +1,54 @@
+//T es el alias del tipo con el que se va  atrabajar
+
+//es decir, que el tipo lo definimos nostros en el momento que querramos
+interface IObserver<T> {
+  refresh(value: T): void;
+}
+
+interface ISubject<T> {
+  observers: IObserver<T>[];
+  subscribe(observer: IObserver<T>): void;
+  unsubscribe(observer: IObserver<T>): void;
+  notify(value: T): void;
+}
+
+class Subject<T> implements ISubject<T> {
+  observers: IObserver<T>[];
+
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(observer: IObserver<T>): void {
+    this.observers.push(observer);
+  }
+
+  unsubscribe(observer: IObserver<T>): void {
+    this.observers = this.observers.filter((obs) => obs !== observer);
+  }
+  notify(value: T): void {
+    this.observers.forEach((e) => {
+      e.refresh(value);
+    });
+  }
+}
+
+class Observer<T> {
+  private fn: (value: T) => void;
+
+  constructor(fn: (value: T) => void) {
+    this.fn = fn;
+  }
+
+  refresh(value: T): void {
+    this.fn(value);
+  }
+}
+
+const subject = new Subject<number>();
+const obs = new Observer<number>((n) => {
+    console.log("Hola" + n)
+})
+
+subject.subscribe(obs);
+subject.notify(1.2)
